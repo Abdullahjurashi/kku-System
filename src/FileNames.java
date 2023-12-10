ba3f5cd34ea92de6f77212e8bc5d9a6d582a79af
Abdullahjurashi <153429528+Abdullahjurashi@users.noreply.github.com>
2023-12-10 14:37:27 +0300

    Add files via upload

---
+++ src/FileNames.java
@@ -0,0 +1,11 @@
+
+
+
+public interface FileNames {
+
+	final static String SESSIONS_FILE = "sessions.data";
+	final static String PRINT_FILE = "printedData.txt";
+	final static String PEOPLE_FILE = "people.data";
+}
+
+
---
+++ src/Person.java
@@ -0,0 +1,57 @@
+
+
+
+
+import java.io.Serializable;
+
+public class Person  implements Serializable{
+	String name;
+	String id;
+	int age;
+	boolean blocked;
+	
+	public Person(String name, String id,int age) {
+		
+		this.setId(id);
+		this.rename(name);
+		this.setAge(age);
+		this.setBlocked(false);
+	}
+	public Person(int age) {
+		this.setAge(age);
+	}
+	@Override
+	public String toString() {
+	return "Person [" + name + "-" + id + "]";
+	}
+	public String name() {
+	return name;
+	}
+	public void rename(String name) {
+	if(name == null || name.isEmpty())
+	name = "na";
+	this.name = name;
+	}
+	public String getId() {
+	return id;
+	}
+	public void setId(String id) {
+	this.id = id;
+	}
+	public int getAge() {
+	return age;
+	}
+	public void setAge(int age) {
+	this.age = age;
+     
+	}
+	public boolean isBlocked() {
+	return blocked;
+	}
+	public void setBlocked(boolean blocked) {
+	this.blocked = blocked;
+	}
+
+
+}
+
